2e1bb90ef33fe54f148fef2ca8ccbf84bed697bc
Amit Paliwal <amitpaliwal259@gmail.com>
2024-03-17 19:44:46 +0530

    update file for the starting position

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,47 @@
-import { Stack, Typography } from "@mui/material";
+import { AuthDialog, CustomeCard, Navbar } from "@/src";
+import { Stack } from "@mui/material";
+import { useState, useCallback } from "react";
 
-export default function Home() {
+const index = () => {
+  const [dialog, setDialog] = useState<{
+    open: boolean;
+    title: string | null;
+  }>({
+    title: null,
+    open: false,
+  });
+
   return (
-    <Stack
-      sx={{
-        height: "100vh",
-        width: "100%",
-        background: 'url()'
-      }}
-    >
-      <Typography variant="h1">Hello</Typography>
-    </Stack>
+    <>
+      <AuthDialog
+        open={dialog.open}
+        close={() => setDialog({ open: false, title: null })}
+        title={dialog.title}
+      />
+      <Stack
+        sx={{
+          height: "100vh",
+          width: "100%",
+          background: `url('/assets/background.png') no-repeat center `,
+          backgroundSize: "cover",
+        }}
+      >
+        <Navbar handleClick={setDialog} />
+        <Stack
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          height={"100%"}
+          width={"100%"}
+        >
+          <CustomeCard
+            image={"/assets/2.svg"}
+            text={"SMART HOME DEVICES for the Safe and Comfortable Life"}
+          />
+        </Stack>
+      </Stack>
+    </>
   );
-}
+};
+
+export default index;
